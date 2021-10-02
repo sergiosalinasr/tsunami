30fb9f95975f54fc8dd632e69d3b7b792ed42866
Sergio Salinas <sergio.salinasr@gmail.com>
2021-10-02 18:04:35 -0300

    Nuevo mensaje a TODOS

--- xbee.py
+++ xbee.py
@@ -2,5 +2,5 @@
 from digi.xbee.devices import XBeeDevice
 device = XBeeDevice("COM6", 115200)
 device.open()
-device.send_data_broadcast("Al que este escuchando: Hello XBee Mundo!")
+device.send_data_broadcast("Nuevo mensaje: Hello XBee TODOS!")
 device.close()
